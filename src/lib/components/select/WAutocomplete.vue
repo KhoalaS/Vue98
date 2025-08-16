7b34b22a366c9278d6905537be2a50f6662e8179
KhoalaS <khoalekappa@gmail.com>
2025-08-16 11:16:06 +0200

    generic options

--- src/lib/components/select/WAutocomplete.vue
+++ src/lib/components/select/WAutocomplete.vue
@@ -1,15 +1,44 @@
-<script setup lang="ts">
+<script setup lang="ts" generic="T extends { id: string; name: string }">
 import { ref } from 'vue'
 import WInput from '../input/WInput.vue'
 import WSelectButton from './WSelectButton.vue'
+
+defineProps<{
+  options?: T[]
+  noneOption?: T
+}>()
+
+const model = defineModel<T>({
+  required: false,
+  default: {
+    id: 'NONE',
+    name: 'None',
+  },
+})
+
+function handleSelect(option: T) {
+  model.value = option
+}
 
 const showSelection = ref(false)
 </script>
 <template>
   <div class="flex flex-col">
-    <WInput>
+    <WInput v-model="model.name">
       <WSelectButton @click="showSelection = !showSelection"></WSelectButton>
     </WInput>
-    <div v-if="showSelection" class="h-12 w-full bg-white border-[1px] border-black"></div>
+    <div v-if="showSelection" class="max-h-[165px] w-full bg-white border-[1px] border-black">
+      <option v-if="noneOption" @click="handleSelect(noneOption)" :value="noneOption.id">
+        {{ noneOption.name }}
+      </option>
+      <option
+        @click="handleSelect(value)"
+        :key="value.id"
+        v-for="value in options ?? []"
+        :value="value.id"
+      >
+        {{ value.name }}
+      </option>
+    </div>
   </div>
 </template>
