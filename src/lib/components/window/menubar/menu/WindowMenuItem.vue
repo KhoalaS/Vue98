a52d4d3c681cc12e4f64faa368a440ded2d86cfb
KhoalaS <khoalekappa@gmail.com>
2026-03-16 21:00:35 +0100

    add simple flag to icons

--- src/lib/components/window/menubar/menu/WindowMenuItem.vue
+++ src/lib/components/window/menubar/menu/WindowMenuItem.vue
@@ -1,10 +1,17 @@
 <script setup lang="ts">
+import { WindowMenuItemIcons, type WindowMenuItemIcon } from './Icons'
 import type { WindowMenuItem } from './WindowMenuItem'
-import WindowMenuItemIcon from './WindowMenuItemIcon.vue'
+import WindowMenuItemIconComponent from './WindowMenuItemIcon.vue'
 
-defineProps<{
+const props = defineProps<{
   menuItem: WindowMenuItem
 }>()
+
+let icon: WindowMenuItemIcon | undefined
+
+if (props.menuItem.icon) {
+  icon = WindowMenuItemIcons[props.menuItem.icon]
+}
 </script>
 <template>
   <tr
@@ -12,10 +19,11 @@ defineProps<{
     class="menu-item h-4.25 w-full text-sm select-none"
     :class="{
       disabled: menuItem.disabled,
+      'simple-icon': icon?.simple,
     }"
   >
     <td>
-      <WindowMenuItemIcon
+      <WindowMenuItemIconComponent
         class="icon"
         :class="{
           disabled: menuItem.disabled,
@@ -23,7 +31,7 @@ defineProps<{
         v-if="menuItem.icon"
         :icon="menuItem.icon"
         :disabled="menuItem.disabled"
-      ></WindowMenuItemIcon>
+      ></WindowMenuItemIconComponent>
     </td>
     <td>
       <span>
@@ -51,10 +59,10 @@ tr.disabled {
   filter: invert(100%);
 }
 
-.menu-item.disabled .icon:deep(img) {
+.menu-item.disabled.simple-icon .icon:deep(img) {
   filter: invert(53%);
 }
-.menu-item.disabled:hover .icon:deep(img) {
+.menu-item.disabled.simple-icon:hover .icon:deep(img) {
   filter: invert(53%);
 }
 </style>
